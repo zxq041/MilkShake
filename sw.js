d928cf3f9fe39bcfb6618739210211d240aa7f9d
zxq041 <zexq2009@gmail.com>
2025-12-01 19:19:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
-const CACHE = "msb-admin-v1";
+const CACHE = "milk-admin-v1";
+
 const ASSETS = [
+  "/admin",
   "/admin.html",
-  "/manifest.json",
+  "/manifest-admin.webmanifest",
   "/favicon.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("activate", e=>{
-  e.waitUntil(self.clients.claim());
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
